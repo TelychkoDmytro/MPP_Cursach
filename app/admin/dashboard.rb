23deb7a88146e6b2369e440d3dbcc12b459db76e
Sof547 <vlasenko2021ks13@student.karazin.ua>
2024-05-29 18:50:56 +0300

    Minor fixes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,24 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel I18n.t('admin.dashboard.latest_10_post_reports') do
+          table_for PostReport.where(status: 'pending').order(created_at: :desc).limit(10) do
+            column :id
+            column :post_id
+            column :user_id
+            column :reason
+            column :reported_at
+            column :status
+            column :created_at
+            column :updated_at
+            column I18n.t('admin.dashboard.actions') do |report|
+              link_to I18n.t('admin.dashboard.view'), admin_post_report_path(report)
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
